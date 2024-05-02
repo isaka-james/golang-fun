3e6e57a73c09f3ab05519eb0be1d6c8113aa8496
Masterplan <76619967+isaka-james@users.noreply.github.com>
2024-05-02 13:46:43 +0300

    hello world golang

---
+++ simple/hello-world.go
@@ -0,0 +1,8 @@
+package main; // tell that this is the entry file
+
+import "fmt"; // importing the package 'fmt' which deals with text formating and printing stuffs on the console
+
+// our main function
+func main(){
+	fmt.Println("hello world"); // We print the 'hello' world
+}
